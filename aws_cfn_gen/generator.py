c67cb1ee10ff3e25aeb8c1f746ea1b4d92f3b037
ohtomi <ohtomi.kenichi@gmail.com>
2016-04-16 22:49:33 +0900

    Add parameter of sub command

--- aws_cfn_gen/generator.py
+++ aws_cfn_gen/generator.py
@@ -6,5 +6,10 @@ from cliff.command import Command
 class Generator(Command):
     '''generate AWS Cloudformation template'''
 
+    def get_parser(self, prog_name):
+        parser = super(Generator, self).get_parser(prog_name)
+        parser.add_argument('data', nargs=1, metavar='<template.py>', help='template script filename')
+        return parser
+
     def take_action(self, parsed_args):
-        print('TODO')
+        self.app.stdout.write('data -> %s\n' % parsed_args.data[0])
