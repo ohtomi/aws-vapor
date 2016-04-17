d4e9fcc55c3d2319a9734d41b422169f7c325ad5
ohtomi <ohtomi.kenichi@gmail.com>
2016-04-17 23:12:39 +0900

    Make config file under user home directory

--- aws_cfn_gen/configure.py
+++ aws_cfn_gen/configure.py
@@ -1,10 +1,30 @@
 # -*- coding: utf-8 -*-
 
 from cliff.command import Command
+from os import mkdir
+from os.path import (exists, expanduser)
+
+import ConfigParser
+
+
+CONFIG_SECTION_DICT = 'dictionary'
+CONFIG_SECTION_VARS = 'variables'
+CONFIG_DIRECTORY = expanduser('~/.aws-cfn-gen')
 
 
 class Configure(Command):
     '''configure this tool'''
 
     def take_action(self, parsed_args):
-        print('TODO')
+        config = ConfigParser.RawConfigParser()
+
+        config.add_section(CONFIG_SECTION_DICT)
+        # TODO
+        config.add_section(CONFIG_SECTION_VARS)
+        # TODO
+
+        if not exists(CONFIG_DIRECTORY):
+            mkdir(CONFIG_DIRECTORY)
+
+        with open(CONFIG_DIRECTORY + '/config', 'wb') as configfile:
+            config.write(configfile)
