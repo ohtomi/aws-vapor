72fdd38cd35c726eb0e108d635b1e86b34b3e1d6
ohtomi <ohtomi.kenichi@gmail.com>
2016-05-07 23:07:10 +0900

    Update example code

--- cfngen/dsl.py
+++ cfngen/dsl.py
@@ -94,9 +94,11 @@ class Resource(Element):
 
     def type(self, name):
         self.attribute(ScalarAttribute('Type', name))
+        return self
 
     def dependsOn(self, resource):
         self.attribute(ReferenceAttribute('DependsOn', resource))
+        return self
 
 
 class Output(Element):
@@ -154,49 +156,40 @@ class ReferenceAttribute(Attribute):
 
 if __name__ == '__main__':
     t = Template(description='Sample Template')
-    t.parameters(
-        Element('KeyName')
-            .attribute_scalar('Description', 'Name of an existing EC2 KeyPair to enable SSH access to the server')
-            .attribute_scalar('Type', 'String')
+    t.parameters(Element('KeyName')
+        .attribute_scalar('Description', 'Name of an existing EC2 KeyPair to enable SSH access to the server')
+        .attribute_scalar('Type', 'String')
     )
-    t.parameters(
-        Element('InstanceType')
-            .attribute_scalar('Description', 'EC2 instance type')
-            .attribute_scalar('Type', 'String')
-            .attribute_scalar('Default', 't1.micro')
+    t.parameters(Element('InstanceType')
+        .attribute_scalar('Description', 'EC2 instance type')
+        .attribute_scalar('Type', 'String')
+        .attribute_scalar('Default', 't1.micro')
     )
-    t.mappings(
-        Element('RegionToAMI')
-            .attribute_list('ap-northeast-1', [
-                ScalarAttribute('AMI1', 'ami-a1bec3a0'),
-                ScalarAttribute('AMI2', 'ami-a1bec3a1')
-            ])
+    t.mappings(Element('RegionToAMI')
+        .attribute_list('ap-northeast-1', [
+            ScalarAttribute('AMI1', 'ami-a1bec3a0'),
+            ScalarAttribute('AMI2', 'ami-a1bec3a1')
+        ])
     )
 
-    vpc = Resource('VPC')
-    vpc.type('AWS::EC2::VPC')
-    vpc.attribute_list('Properties', [
+    vpc = Resource('VPC').type('AWS::EC2::VPC').attribute_list('Properties', [
         ScalarAttribute('CidrBlock', '10.104.0.0/16'),
         ScalarAttribute('InstanceTenancy', 'default')
     ])
     t.resources(vpc)
 
-    igw = Resource('InternetGateway')
-    igw.type('AWS:EC2::InternetGateway')
+    igw = Resource('InternetGateway').type('AWS:EC2::InternetGateway')
     t.resources(igw)
 
-    attachIgw = Resource('AttachInternetGateway')
-    attachIgw.type('AWS::EC2::VPCGatewayAttachment')
-    attachIgw.attribute_list('Properties', [
+    attachIgw = Resource('AttachInternetGateway').type('AWS::EC2::VPCGatewayAttachment').attribute_list('Properties', [
         ReferenceAttribute('VpcId', vpc),
         ReferenceAttribute('InternetGatewayId', igw)
     ])
     t.resources(attachIgw)
 
-    t.outputs(
-        Element('VpcId')
-            .attribute_scalar('Description', '-')
-            .attribute_reference('Value', vpc)
+    t.outputs(Element('VpcId')
+        .attribute_scalar('Description', '-')
+        .attribute_reference('Value', vpc)
     )
 
     from json import dumps
