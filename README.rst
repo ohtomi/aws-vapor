f4948957e1922026adb4cab2927a8c760afa4f79
ohtomi <ohtomi.kenichi@gmail.com>
2016-07-19 22:58:03 +0900

    Convert readme from md to reST

---
+++ README.rst
@@ -0,0 +1,39 @@
+****************************************
+aws-cloudformation-generator
+****************************************
+
+Description
+========================================
+
+This tool generates AWS CloudFormation template from python object.
+
+Requirements
+========================================
+
+- Python v2.7.x
+
+How to install
+========================================
+
+```bash
+$ pip install git+https://github.com/ohtomi/aws-cloudformation-generator
+```
+
+How to use
+========================================
+
+TODO
+
+Contributing
+========================================
+
+1. Fork it!
+2. Create your feature branch: `git checkout -b my-new-feature`
+3. Commit your changes: `git commit -am 'Add some feature'`
+4. Push to the branch: `git push origin my-new-feature`
+5. Submit a pull request :D
+
+License
+========================================
+
+MIT
--- setup.py
+++ setup.py
@@ -1,10 +1,26 @@
+from os import path
 from setuptools import setup, find_packages
+import codecs
+
 
 setup(
     name='aws-vapor',
     version='0.0.1',
+    description='Generates AWS CloudFormation template from python object',
+    long_description=codecs.open(
+        path.join(path.abspath(path.dirname(__file__)), 'README.rst'),
+        mode='r',
+        encoding='utf-8'
+    ).read(),
+    author='Kenichi Ohtomi',
+    author_email='ohtomi.kenichi@gmail.com',
+    url='https://github.com/ohtomi/aws-cloudformation-generator/',
+    #download_url='https://github.com/ohtomi/aws-cloudformation-generator/tarball/0.0.1',
+    keywords='aws cloudformation template generator',
     packages=find_packages(),
-    install_requires=['cliff'],
+    install_requires=[
+        'cliff',
+    ],
     entry_points={
         'console_scripts':
             'aws-vapor = aws_vapor.main:main',
@@ -15,10 +31,14 @@ setup(
     },
     zip_safe=False,
     classifiers=[
-          'Environment :: Console',
-          'Intended Audience :: Developers',
-          'Operating System :: MacOS :: MacOS X',
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 2.7',
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
     ],
 )
