5a424d1448ed878201f132d00aff0f83d920e85f
ohtomi <ohtomi.kenichi@gmail.com>
2016-07-23 01:55:16 +0900

    Formatting

--- aws_vapor/configure.py
+++ aws_vapor/configure.py
@@ -11,13 +11,16 @@ class Configure(Command):
     def get_parser(self, prog_name):
         parser = super(Configure, self).get_parser(prog_name)
         subparsers = parser.add_subparsers(help='sub-command', title='sub-commands')
+
         list_subparser = subparsers.add_parser('list', help='lists all values within config file')
         list_subparser.set_defaults(func=self.list_configuration)
+
         set_subparser = subparsers.add_parser('set', help='sets key to specified value')
         set_subparser.set_defaults(func=self.set_configuration)
         set_subparser.add_argument('section')
         set_subparser.add_argument('key')
         set_subparser.add_argument('value')
+
         return parser
 
     def take_action(self, args):
