99b4e3e098ea53b84dc9965848a7a9bb001a6a20
ohtomi <ohtomi.kenichi@gmail.com>
2016-07-26 23:17:47 +0900

    Remove old example

--- examples/my_script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -xe
-yum update -y
-yum update -y aws-cfn-bootstrap
-/opt/aws/bin/cfn-init -v --stack {{ stack_id }} --resource {{ resource_name }} --region {{ region }}
-/opt/aws/bin/cfn-signal -e $? --stack {{ stack_id }} --resource {{ resource_name }} --region {{ region }}
