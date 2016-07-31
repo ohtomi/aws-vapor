409b0c433d8c08906de8884e9a6c048ed662da3b
ohtomi <ohtomi.kenichi@gmail.com>
2016-08-01 00:20:54 +0900

    Update example

--- examples/contribs/add_mapping.py
+++ examples/contribs/add_mapping.py
@@ -8,6 +8,10 @@ from aws_vapor.dsl import (
 
 
 def recipe(t):
-    t.mappings(Mapping('RegionToAMI')
-        .add_category('us-east-1').add_item('AMI', 'ami-xxxxxxxx')
+    t.mappings(Mapping('RegionMap')
+        .add_category('eu-west-1').add_item('AMI', 'ami-24506250').add_item('TestAZ', 'eu-west-1a')
+        .add_category('sa-east-1').add_item('AMI', 'ami-3e3be423').add_item('TestAZ', 'sa-east-1a')
+        .add_category('ap-southeast-1').add_item('AMI', 'ami-74dda626').add_item('TestAZ', 'ap-southeast-1a')
+        .add_category('ap-southeast-2').add_item('AMI', 'ami-b3990e89').add_item('TestAZ', 'ap-southeast-2a')
+        .add_category('ap-northeast-1').add_item('AMI', 'ami-dcfa4edd').add_item('TestAZ', 'ap-northeast-1a')
     , merge=True)
