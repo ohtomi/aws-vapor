27600c1dbf1e4dccee8914cc3ba587e7a87bc664
ohtomi <ohtomi.kenichi@gmail.com>
2017-06-10 11:34:38 +0900

    Add support Python v3.6.x

--- setup.py
+++ setup.py
@@ -75,6 +75,7 @@ def main():
             'Programming Language :: Python :: 2.7',
             'Programming Language :: Python :: 3.4',
             'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
         ],
     )
 
