ecc2718d27a6dc2b048837955db74fab327f4af6
ohtomi <ohtomi.kenichi@gmail.com>
2017-06-22 21:14:32 +0900

    Update api document

--- aws_vapor/configure.py
+++ aws_vapor/configure.py
@@ -26,10 +26,23 @@ class Configure(Command):
         return parser
 
     def take_action(self, args):
-        """Show the current configuration or set a new configuration."""
+        """Show the current configuration or set a new configuration.
+
+        The first parsed command line argument is a sub command name.
+        Show the current configuration if the sub command is `list`,
+        Or set a new configuration if the sub command is `set`.
+
+        Args:
+            args (dict): `func` is a method object of this class.
+        """
         args.func(args)
 
     def list_configuration(self, args):
+        """Show the current configuration.
+
+        Args:
+            args (dict): not be used.
+        """
         props = utils.load_from_config_file()
         for section, entries in list(props.items()):
             self.app.stdout.write('[{0}]\n'.format(section))
@@ -37,6 +50,17 @@ class Configure(Command):
                 self.app.stdout.write('{0} = {1}\n'.format(key, value))
 
     def set_configuration(self, args):
+        """Set a new configuration.
+
+        Args:
+            args (dict): `system` is a flag whether or not a new configuration will be saved globaly.
+
+                         `section` is a name of configuration section block.
+
+                         `key` is a name of configuration property.
+
+                         `value` is a value of configuration property.
+        """
         save_on_global = args.system
 
         config_directory = [utils.GLOBAL_CONFIG_DIRECTORY] if save_on_global else [utils.LOCAL_CONFIG_DIRECTORY]
