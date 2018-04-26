0ae1d58d80aec32cf1d0d546b03e6371351784ac
ohtomi <ohtomi.kenichi@gmail.com>
2018-04-26 21:47:57 +0900

    Update copyright in api document

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'aws-vapor'
-copyright = u'2017, Kenichi Ohtomi'
+copyright = u'2018, Kenichi Ohtomi'
 author = u'Kenichi Ohtomi'
 
 # The version info for the project you're documenting, acts as replacement for
