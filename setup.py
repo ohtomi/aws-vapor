b715f46d717f6626328323c6040ca0cded337571
ohtomi <ohtomi.kenichi@gmail.com>
2018-04-27 22:48:45 +0900

    Eliminate old python support

--- setup.py
+++ setup.py
@@ -72,9 +72,6 @@ def main():
             'Operating System :: Microsoft :: Windows',
             'Operating System :: POSIX',
             'Programming Language :: Python',
-            'Programming Language :: Python :: 2.7',
-            'Programming Language :: Python :: 3.4',
-            'Programming Language :: Python :: 3.5',
             'Programming Language :: Python :: 3.6',
         ],
     )
